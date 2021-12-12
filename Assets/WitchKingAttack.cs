1039de69e31ac5a288665387f36c20b666e67a20
DannyIsYog <fabiodaniel.2000@gmail.com>
2021-12-12 01:15:52 +0000

    Fixed attack animation (walk animation still bugged if in same spot as player

--- Assets/WitchKingAttack.cs
+++ Assets/WitchKingAttack.cs
@@ -2,12 +2,10 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class WitchKingRun : StateMachineBehaviour
+public class WitchKingAttack : StateMachineBehaviour
 {
-
     Transform _player;
     Entity entity;
-
     //OnStateEnter is called when a transition starts and the state machine starts to evaluate this state
     override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
     {
@@ -18,34 +16,25 @@ public class WitchKingRun : StateMachineBehaviour
     //OnStateUpdate is called on each Update frame between OnStateEnter and OnStateExit callbacks
     override public void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
     {
-        float offset = _player.position.x - animator.transform.position.x;
-        float side = (offset) / Mathf.Abs(offset);
-        if (Vector2.Distance(_player.position, entity.transform.position) <= 0.5)
-            entity.SetMovement(1);
-        else
-            entity.SetMovement(side);
         Vector2 hitboxPos = entity.transform.position - new Vector3(entity.getAttackHitbox().offset, 0, 0);
 
         if (Vector2.Distance(_player.position, hitboxPos) <= entity.getAttackHitbox().range)
-        {
-            entity.SetMovement(0);
-            animator.SetTrigger("Attack");
-        }
-
-
+            animator.SetBool("KeepAttacking", true);
+        else
+            animator.SetBool("KeepAttacking", false);
     }
 
     //OnStateExit is called when a transition ends and the state machine finishes evaluating this state
     override public void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
     {
-        animator.ResetTrigger("Attack");
+
     }
 
-    // OnStateMove is called right after Animator.OnAnimatorMove()
-    //override public void OnStateMove(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
-    //{
-    //    // Implement code that processes and affects root motion
-    //}
+    /*OnStateMove is called right after Animator.OnAnimatorMove()
+    override public void OnStateMove(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+       // Implement code that processes and affects root motion
+    }*/
 
     // OnStateIK is called right after Animator.OnAnimatorIK()
     //override public void OnStateIK(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)

a41f230b84fec1cea781ad553a5999c003274171
DannyIsYog <fabiodaniel.2000@gmail.com>
2021-12-12 18:31:43 +0000

    Audio done in start scene

--- Assets/Scripts/Intro.cs
+++ Assets/Scripts/Intro.cs
@@ -5,6 +5,8 @@ using UnityEngine.SceneManagement;
 public class Intro : MonoBehaviour
 {
     [SerializeField] private GameObject[] _textScreens;
+
+    [SerializeField] private AudioSource[] _audios;
     [SerializeField] private float _delay;
     [SerializeField] private string _scene;
 
@@ -13,7 +15,8 @@ public class Intro : MonoBehaviour
     private CanvasRenderer _current;
     private int _fade = 1;
 
-    void Start() {
+    void Start()
+    {
         foreach (GameObject screen in _textScreens)
         {
             CanvasRenderer canvasRenderer = screen.GetComponent<CanvasRenderer>();
@@ -25,7 +28,7 @@ public class Intro : MonoBehaviour
 
     void Update()
     {
-        if (_current != null) 
+        if (_current != null)
         {
             _timer += Time.deltaTime * _fade;
             float alpha = Mathf.Lerp(0f, 1f, _timer / _fadeTime);
@@ -33,18 +36,30 @@ public class Intro : MonoBehaviour
         }
     }
 
-    IEnumerator ShowScreens() {
+    IEnumerator ShowScreens()
+    {
+        int i = 0;
+        bool playingAudio = false;
         foreach (GameObject screen in _textScreens)
         {
             _current = screen.GetComponent<CanvasRenderer>();
+            if (_audios != null && _audios.Length > i)
+            {
+                _audios[i].Play();
+                playingAudio = true;
+            }
             screen.SetActive(true);
             _fade = 1;
             yield return new WaitForSeconds(_fadeTime);
             _fade = 0;
             yield return new WaitForSeconds(_delay);
+            if (playingAudio)
+                yield return new WaitWhile(() => _audios[i].isPlaying);
             _fade = -1;
             yield return new WaitForSeconds(_fadeTime);
             screen.SetActive(false);
+            i++;
+            playingAudio = false;
         }
 
         SceneManager.LoadScene(_scene);
